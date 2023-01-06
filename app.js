7d5722ec724c824fbd4920c919bd257acf0ab869
Don Creamy <webjellystudios@gmail.com>
2023-01-06 17:27:39 -0500

    Working choice for user and computer

--- app.js
+++ app.js
@@ -6,5 +6,18 @@ const possibleChoices = document.querySelectorAll('button')
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
   userChoice = e.target.id
   userChoiceDisplay.innerHTML = userChoice
+  generateComputerChoice()
 }))
+// Generate a random # for the computer's choice
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1
 
+  if(randomNumber === 1) {
+    computerChoice = 'rock'
+  } else if (randomNumber === 2) {
+    computerChoice = 'paper'
+  } else {
+    computerChoice = 'scissors'
+  }
+  computerChoiceDisplay.innerHTML = computerChoice 
+}
